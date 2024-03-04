160f31183a2507ce89ccc3e685243dccb72d4602
Jackson Blankenship <jacksblan@gmail.com>
2024-03-04 13:36:09 -0600

    feat: improve example console log

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,7 @@
 import { env } from './utils/env';
 
 (async () => {
-  console.log(`[node-starter] Application is running`);
   console.log(
-    `[node-starter] Example environment variable (EXAMPLE_VAR): ${env.EXAMPLE_VAR}`,
+    `🔧 Using environment variable (EXAMPLE_VAR): ${env.EXAMPLE_VAR}`,
   );
 })();
