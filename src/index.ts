df56b36631d0a108c4d91ea1bcc6672409b0fa51
Jackson Blankenship <jacksblan@gmail.com>
2024-03-04 13:33:24 -0600

    feat: remove pino

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import { env } from './utils/env';
-import logger from './utils/logger';
 
 (async () => {
-  logger.info(env, 'Hello, world!!');
+  console.log(`[node-starter] Application is running`);
+  console.log(
+    `[node-starter] Example environment variable (EXAMPLE_VAR): ${env.EXAMPLE_VAR}`,
+  );
 })();
